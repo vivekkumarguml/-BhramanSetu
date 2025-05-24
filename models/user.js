d3817cb4855b408f061aaed720e52eb53c453c03
VIVEK KUMAR <vivekkumarprofessional165@gmail.com>
2025-05-24 13:25:29 +0530

    added core funcationalities

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose =require("passport-local-mongoose");
+
+const userSchema =new Schema({
+    email:{
+        type:String,
+        required:true
+    }
+});
+
+
+userSchema.plugin(passportLocalMongoose);  // it will add username ,hascode and salting automatically 
+
+module.exports = mongoose.model('User', userSchema);
